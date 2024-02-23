0d8d8f0b508cd5ce7cfb8f8969611fc01a1979be
Eric Lifs <lifszyc.eric@gmail.com>
2024-02-23 16:13:54 -0300

    feat: add create todo functionality

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,10 +1,38 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View } from 'react-native';
+
+import Button from '~/components/button';
+import TextInput from '~/components/text-input';
+import { supabase } from '~/utils/storage';
 
 const Index = () => {
+  const [todo, setTodo] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onAddTodo = async () => {
+    setIsLoading(true);
+
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+
+    const newTodo = {
+      user_id: user?.id,
+      task: todo,
+      is_complete: false,
+    };
+
+    await supabase.from('todos').insert(newTodo).select().single();
+
+    setIsLoading(false);
+    setTodo('');
+  };
+
   return (
-    <View>
-      <Text>Index</Text>
+    <View className="flex-1">
+      <TextInput value={todo} onChangeText={setTodo} />
+
+      <Button onPress={onAddTodo} text="Add todo" disabled={!todo || isLoading} />
     </View>
   );
 };
