be69b68e85d033c5129396dc317d4926833b242d
Eric Lifs <lifszyc.eric@gmail.com>
2024-02-23 12:06:04 -0300

    feat: add styles to tabs navigator

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,9 +1,50 @@
+import { Ionicons } from '@expo/vector-icons';
 import { Tabs } from 'expo-router';
 import React from 'react';
-import { View, Text } from 'react-native';
+import { TouchableOpacity } from 'react-native';
+import colors from 'tailwindcss/colors';
+
+import { supabase } from '~/utils/storage';
 
 const Layout = () => {
-  return <Tabs />;
+  return (
+    <Tabs
+      screenOptions={{
+        headerShadowVisible: false,
+        headerStyle: {
+          backgroundColor: colors.zinc[900],
+        },
+        headerTintColor: colors.white,
+        tabBarActiveTintColor: colors.white,
+        tabBarStyle: {
+          backgroundColor: colors.zinc[900],
+        },
+        headerRight: () => (
+          <TouchableOpacity onPress={() => supabase.auth.signOut()} className="mr-5">
+            <Ionicons name="log-out-outline" size={24} color={colors.white} />
+          </TouchableOpacity>
+        ),
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: (props) => (
+            <Ionicons name={props.focused ? 'home-sharp' : 'home-outline'} {...props} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+          tabBarIcon: (props) => (
+            <Ionicons name={props.focused ? 'person-sharp' : 'person-outline'} {...props} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 };
 
 export default Layout;
