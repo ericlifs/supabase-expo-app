26ce2d7148f2d5ad4244b0479af0d8d06e9a3cb6
Eric Lifs <lifszyc.eric@gmail.com>
2024-02-24 17:51:50 -0300

    feat: add picking new user avatar

--- app/(auth)/profile.tsx
+++ app/(auth)/profile.tsx
@@ -1,8 +1,11 @@
+import { decode } from 'base64-arraybuffer';
+import * as FileSystem from 'expo-file-system';
 import * as ImagePicker from 'expo-image-picker';
 import React, { useState } from 'react';
 import { View, Image } from 'react-native';
 
 import Button from '~/components/button';
+import { supabase } from '~/utils/storage';
 
 const Profile = () => {
   const [image, setImage] = useState<string | null>(null);
@@ -16,6 +19,23 @@ const Profile = () => {
 
     if (!result.canceled) {
       setImage(result.assets[0].uri);
+
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+
+      const [pickedImage] = result.assets;
+      const base64 = await FileSystem.readAsStringAsync(pickedImage.uri, {
+        encoding: FileSystem.EncodingType.Base64,
+      });
+      const filePath = `${user?.id}/avatar.png`;
+      const contentType = 'image/png';
+
+      try {
+        await supabase.storage.from('avatars').upload(filePath, decode(base64), { contentType });
+      } catch (err) {
+        console.log(err);
+      }
     }
   };
 
