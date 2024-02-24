2114ac7c03db1ab37cd6f9cd0455fc3ab59fdc30
Eric Lifs <lifszyc.eric@gmail.com>
2024-02-24 16:51:01 -0300

    feat: add showing all todos

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,12 +1,22 @@
-import React, { useState } from 'react';
-import { View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import React, { useCallback, useEffect, useState } from 'react';
+import { FlatList, View, ListRenderItem, Text } from 'react-native';
+import colors from 'tailwindcss/colors';
 
 import Button from '~/components/button';
 import TextInput from '~/components/text-input';
 import { supabase } from '~/utils/storage';
 
+interface Todo {
+  id: number;
+  user_id: number;
+  task: string;
+  is_complete: boolean;
+}
+
 const Index = () => {
   const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState<Todo[]>([]);
   const [isLoading, setIsLoading] = useState(false);
 
   const onAddTodo = async () => {
@@ -22,15 +32,38 @@ const Index = () => {
       is_complete: false,
     };
 
-    await supabase.from('todos').insert(newTodo).select().single();
+    const { data } = await supabase.from('todos').insert(newTodo).select().single();
 
-    setIsLoading(false);
     setTodo('');
+    setIsLoading(false);
+    setTodos((prevTodos) => [...prevTodos, data]);
   };
 
+  const getAllTodos = useCallback(async () => {
+    const { data, error } = await supabase
+      .from('todos')
+      .select('*')
+      .order('inserted_at', { ascending: false });
+
+    if (!error) {
+      setTodos(data);
+    }
+  }, []);
+
+  useEffect(() => {
+    getAllTodos();
+  }, [getAllTodos]);
+
+  const renderTodo: ListRenderItem<Todo> = ({ item }) => (
+    <View className="flex-row p-5 items-center">
+      <Text className="flex-1 mr-4">{item.task}</Text>
+      {item.is_complete && <Ionicons name="checkmark" color={colors.lime[800]} size={30} />}
+    </View>
+  );
+
   return (
-    <View className="flex-1 bg-zinc-900 p-5">
-      <View className="w-full flex flex-row">
+    <View className="flex-1">
+      <View className="w-full flex flex-row bg-zinc-900 p-5">
         <TextInput value={todo} onChangeText={setTodo} className="w-auto flex-1 mr-4" />
 
         <Button
@@ -41,6 +74,14 @@ const Index = () => {
           className="w-auto"
         />
       </View>
+
+      <FlatList
+        data={todos}
+        contentContainerClassName="bg-zinc-700"
+        ItemSeparatorComponent={() => <View className="h-px mx-5 flex-1 bg-zinc-600" />}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={renderTodo}
+      />
     </View>
   );
 };
