7c519513c4d1c7e8be383a8944b17310ad1e8644
Eric Lifs <lifszyc.eric@gmail.com>
2024-02-24 17:24:28 -0300

    feat: add avatar image in profile screen

--- app/(auth)/profile.tsx
+++ app/(auth)/profile.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import React, { useState } from 'react';
+import { View, Image } from 'react-native';
+
+import Button from '~/components/button';
 
 const Profile = () => {
+  const [image, setImage] = useState<string | null>(null);
+
+  const pickImage = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
   return (
-    <View>
-      <Text>Profile</Text>
+    <View className="flex-1 px-5 items-center">
+      <Image
+        source={{ uri: image! }}
+        className="h-24 w-24 rounded-full bg-slate-500 m-10 self-center"
+      />
+      <Button text="Pick avatar image" onPress={pickImage} />
     </View>
   );
 };
